8d229af892a06af1a80ec577e659605b02966a4b
walter_rz <wremache@uazuay.edu.ec>
2021-06-02 17:00:33 -0500

    Prueba higcharts

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/efx-app-web-marketplace'));
+app.use(express.static(__dirname + '/grupofaro'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/efx-app-web-marketplace/index.html'));
+    res.sendFile(path.join(__dirname + '/grupofaro/column-basic/index.html'));
 });
 
 app.listen(process.env.PORT || 3000);
